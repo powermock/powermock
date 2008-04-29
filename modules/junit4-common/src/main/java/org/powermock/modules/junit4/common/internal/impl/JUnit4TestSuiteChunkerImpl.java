d0b83b211ed72d32f0009ae223055831ded93f21
johanhaleby <johanhaleby@99b96964-f955-0410-9471-33fff0e6aa07>
2008-04-29 20:18:37 +0000

    Solved merge conflicts

--- modules/junit4-common/src/main/java/org/powermock/modules/junit4/common/internal/impl/JUnit4TestSuiteChunkerImpl.java
+++ modules/junit4-common/src/main/java/org/powermock/modules/junit4/common/internal/impl/JUnit4TestSuiteChunkerImpl.java
@@ -17,6 +17,7 @@ package org.powermock.modules.junit4.common.internal.impl;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -33,24 +34,19 @@ import org.junit.Test;
 import org.junit.runner.Description;
 import org.junit.runner.notification.RunNotifier;
 
-public class JUnit4TestSuiteChunkerImpl extends
-		AbstractTestSuiteChunkerImpl<PowerMockJUnitRunnerDelegate> implements
-		JUnit4TestSuiteChunker {
+public class JUnit4TestSuiteChunkerImpl extends AbstractTestSuiteChunkerImpl<PowerMockJUnitRunnerDelegate> implements JUnit4TestSuiteChunker {
 
 	private Description description;
 	private final Class<? extends PowerMockJUnitRunnerDelegate> runnerDelegateImplementationType;
 
-	public JUnit4TestSuiteChunkerImpl(
-			Class<?> testClass,
-			Class<? extends PowerMockJUnitRunnerDelegate> runnerDelegateImplementationType) {
+	public JUnit4TestSuiteChunkerImpl(Class<?> testClass, Class<? extends PowerMockJUnitRunnerDelegate> runnerDelegateImplementationType) {
 		super(testClass);
 		if (testClass == null) {
 			throw new IllegalArgumentException("You must supply a test class");
 		}
 
 		if (runnerDelegateImplementationType == null) {
-			throw new IllegalArgumentException(
-					"Runner delegate type cannot be null.");
+			throw new IllegalArgumentException("Runner delegate type cannot be null.");
 		}
 
 		this.runnerDelegateImplementationType = runnerDelegateImplementationType;
@@ -65,31 +61,28 @@ public class JUnit4TestSuiteChunkerImpl extends
 
 	public void run(RunNotifier notifier) {
 		Set<Entry<MockClassLoader, List<Method>>> entrySet = getAllChunkEntries();
-		Iterator<Entry<MockClassLoader, List<Method>>> iterator = entrySet
-				.iterator();
+		Iterator<Entry<MockClassLoader, List<Method>>> iterator = entrySet.iterator();
 
 		if (delegates.size() != getChunkSize()) {
-			throw new IllegalStateException(
-					"Internal error: There must be an equal number of suites and delegates.");
+			throw new IllegalStateException("Internal error: There must be an equal number of suites and delegates.");
 		}
 
 		for (PowerMockJUnitRunnerDelegate delegate : delegates) {
 			Entry<MockClassLoader, List<Method>> next = iterator.next();
-			PowerMockRunListener powerMockListener = new PowerMockRunListener(
-					next.getKey());
+			PowerMockRunListener powerMockListener = new PowerMockRunListener(next.getKey());
 			notifier.addListener(powerMockListener);
 			delegate.run(notifier);
 		}
 	}
 
 	public boolean shouldExecuteTestForMethod(Method potentialTestMethod) {
-		return potentialTestMethod.isAnnotationPresent(Test.class)
+		return (potentialTestMethod.getName().startsWith("test") && Modifier.isPublic(potentialTestMethod.getModifiers())
+				&& potentialTestMethod.getReturnType().equals(Void.TYPE) || potentialTestMethod.isAnnotationPresent(Test.class))
 				&& !potentialTestMethod.isAnnotationPresent(Ignore.class);
 	}
 
 	@Override
-	protected PowerMockJUnitRunnerDelegate createDelegatorFromClassloader(
-			MockClassLoader classLoader, Class<?> testClass,
+	protected PowerMockJUnitRunnerDelegate createDelegatorFromClassloader(MockClassLoader classLoader, Class<?> testClass,
 			final List<Method> methodsToTest) throws Exception {
 
 		Set<String> methodNames = new HashSet<String>();
@@ -97,15 +90,11 @@ public class JUnit4TestSuiteChunkerImpl extends
 			methodNames.add(method.getName());
 		}
 
-		final Class<?> testClassLoadedByMockedClassLoader = classLoader
-				.loadClass(testClass.getName());
-		Class<?> delegateClass = classLoader
-				.loadClass(runnerDelegateImplementationType.getName());
-		Constructor<?> con = delegateClass.getConstructor(new Class[] {
-				Class.class, String[].class });
-		return (PowerMockJUnitRunnerDelegate) con.newInstance(new Object[] {
-				testClassLoadedByMockedClassLoader,
-				methodNames.toArray(new String[0]) });
+		final Class<?> testClassLoadedByMockedClassLoader = classLoader.loadClass(testClass.getName());
+		Class<?> delegateClass = classLoader.loadClass(runnerDelegateImplementationType.getName());
+		Constructor<?> con = delegateClass.getConstructor(new Class[] { Class.class, String[].class });
+		return (PowerMockJUnitRunnerDelegate) con
+				.newInstance(new Object[] { testClassLoadedByMockedClassLoader, methodNames.toArray(new String[0]) });
 	}
 
 	public synchronized int getTestCount() {
@@ -121,8 +110,7 @@ public class JUnit4TestSuiteChunkerImpl extends
 	public Description getDescription() {
 		if (description == null) {
 			if (delegates.size() == 0) {
-				throw new IllegalStateException(
-						"Internal error: Run delegates were 0.");
+				throw new IllegalStateException("Internal error: Run delegates were 0.");
 			}
 
 			// Use the first delegator as the base for the description.
@@ -136,8 +124,7 @@ public class JUnit4TestSuiteChunkerImpl extends
 			 */
 			for (int i = 1; i < delegates.size(); i++) {
 				// Get the method-level descriptions
-				ArrayList<Description> children = delegates.get(i)
-						.getDescription().getChildren();
+				ArrayList<Description> children = delegates.get(i).getDescription().getChildren();
 				// Add all method-level descriptions to the main description.
 				for (Description methodDescription : children) {
 					description.addChild(methodDescription);
