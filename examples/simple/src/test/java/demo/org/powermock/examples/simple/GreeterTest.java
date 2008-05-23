d54c3c8c1b2efd35d21d6b5b7ba4f0abf9257c11
johanhaleby <johanhaleby@99b96964-f955-0410-9471-33fff0e6aa07>
2008-05-23 16:23:00 +0000

    Enabled a test since the functionality required by the test is now implemented

--- examples/simple/src/test/java/demo/org/powermock/examples/simple/GreeterTest.java
+++ examples/simple/src/test/java/demo/org/powermock/examples/simple/GreeterTest.java
@@ -9,7 +9,6 @@ import static org.powermock.PowerMock.replay;
 import static org.powermock.PowerMock.verify;
 import static org.powermock.Whitebox.invokeMethod;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
@@ -18,32 +17,31 @@ import org.powermock.modules.junit4.PowerMockRunner;
 
 @RunWith(PowerMockRunner.class)
 @SuppressStaticInitializationFor("demo.org.powermock.examples.simple.SimpleConfig")
-@PrepareForTest({Greeter.class, Logger.class})
+@PrepareForTest( { Greeter.class, Logger.class })
 public class GreeterTest {
 
-    @Test
-    public void testGetMessage() {
-    	mockStatic(SimpleConfig.class);
-    	expect(SimpleConfig.getGreeting()).andReturn("Hi");
-    	expect(SimpleConfig.getTarget()).andReturn("All");
-    	replay(SimpleConfig.class);
-    	
-    	assertEquals("Hi All", invokeMethod(Greeter.class, "getMessage"));
-    	
-    	verify(SimpleConfig.class);
-    }
-    
-    @Test
-    @Ignore
-    public void testRun() {
-    	Logger logger = mockConstruction(Logger.class);
-    	logger.log("Hello");
-    	expectLastCall().times(10);
-    	replay(logger);
-    	
-    	invokeMethod(new Greeter(), "run", 10, "Hello");
-    	
-    	verify(logger);
-    }
-    
+	@Test
+	public void testGetMessage() {
+		mockStatic(SimpleConfig.class);
+		expect(SimpleConfig.getGreeting()).andReturn("Hi");
+		expect(SimpleConfig.getTarget()).andReturn("All");
+		replay(SimpleConfig.class);
+
+		assertEquals("Hi All", invokeMethod(Greeter.class, "getMessage"));
+
+		verify(SimpleConfig.class);
+	}
+
+	@Test
+	public void testRun() {
+		Logger logger = mockConstruction(Logger.class);
+		logger.log("Hello");
+		expectLastCall().times(10);
+		replay(logger);
+
+		invokeMethod(new Greeter(), "run", 10, "Hello");
+
+		verify(logger);
+	}
+
 }
