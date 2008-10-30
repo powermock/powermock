360d45b945538ecb160dfbe42803ca14b3cfdb43
johan.haleby <johan.haleby@99b96964-f955-0410-9471-33fff0e6aa07>
2008-10-30 09:29:52 +0000

    removed unused dependencies

--- core/src/test/java/org/powermock/WhiteBoxTest.java
+++ core/src/test/java/org/powermock/WhiteBoxTest.java
@@ -15,8 +15,8 @@
  */
 package org.powermock;
 
-import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
