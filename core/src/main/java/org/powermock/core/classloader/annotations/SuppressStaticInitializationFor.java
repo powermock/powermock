e3ca926a9602f0a03a669d245b853740e67907c9
johan.haleby <johan.haleby@99b96964-f955-0410-9471-33fff0e6aa07>
2008-11-04 13:07:11 +0000

    Updated java doc

--- core/src/main/java/org/powermock/core/classloader/annotations/SuppressStaticInitializationFor.java
+++ core/src/main/java/org/powermock/core/classloader/annotations/SuppressStaticInitializationFor.java
@@ -7,7 +7,17 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-// TODO Should also support methods so that it'll work with chunking..
+/**
+ * Use this annotation to suppress static initializers (constructors) for one or
+ * more classes.
+ * <p>
+ * The reason why an annotation is needed for this is because we need to know at
+ * <strong>load-time</strong> if the static constructor execution for this
+ * class should be skipped or not. Unfortunately we cannot pass the class as the
+ * value parameter to the annotation (and thus get type-safe values) because
+ * then the class would be loaded before PowerMock could have suppressed its
+ * constructor.
+ */
 @Target( { ElementType.TYPE, ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
--- modules/junit4/src/test/java/org/powermock/modules/junit4/suppressconstructor/SuppressConstructorDemoTest.java
+++ modules/junit4/src/test/java/org/powermock/modules/junit4/suppressconstructor/SuppressConstructorDemoTest.java
@@ -32,36 +32,17 @@ import org.junit.runner.RunWith;
 import samples.suppressconstructor.SuppressConstructorDemo;
 import samples.suppressconstructor.SuppressConstructorSubclassDemo;
 
-
 /**
  * This test demonstrates how to tell PowerMock to avoid executing constructor
  * code for a certain class. This is crucial in certain tests where the
  * constructor or a subclass's constructor performs operations that are of no
  * concern to the unit test of the actual class or if the constructor performs
  * operations, such as getting services from a runtime environment that has not
- * been initialized. For example in an SWT application, a parent class may
- * depend on that the SWT runtime environment has been started and if not you'll
- * get an exception. This makes it impossible to unit test such classes without
- * creating an instance of the class that doesn't call the actual constructor
- * code (which is not possible without byte-code manipulation). In normal
- * situations you're forced to create an integration or function test for the
- * class instead (and thus the runtime environment is available). This is not
- * particularly good when it comes to testing method logic. PowerMock solves
- * these problems by letting you specify the
+ * been initialized. In normal situations you're forced to create an integration
+ * or function test for the class instead (and thus the runtime environment is
+ * available). This is not particularly good when it comes to testing method
+ * logic. PowerMock solves these problems by letting you specify the
  * {@link PowerMock#suppressConstructorCode(Class...)} method
- * <p>
- * Nedanstående ska in till SuppressStaticInitializer annoteringen, därför står
- * det kvar: Note: The reason that an annotation is needed is because we need to
- * know at <strong>load-time</strong> if the constructor execution for this
- * class should be skipped or not. Unfortunately we cannot pass the class as the
- * value parameter to the <code>SkipExecutingConstructorCodeFor</code>
- * annotation (and thus get type-safe values) because in that case the class is
- * loaded when the PowerMock class loader checks if the
- * <code>SkipExecutingConstructorCodeFor</code> is present making it
- * impossible to know in advance if the class should skip executing constructor
- * code or not.
- * 
- * @author Johan Haleby
  */
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(SuppressConstructorDemo.class)
@@ -74,11 +55,8 @@ public class SuppressConstructorDemoTest {
 	@Test
 	public void testSuppressParentConstructor() throws Exception {
 		suppressConstructorCode(SuppressConstructorSubclassDemo.class);
-		final SuppressConstructorDemo tested = new SuppressConstructorDemo(
-				"a message");
-		assertNull(
-				"Message should have been null since we're skipping the execution of the constructor code.",
-				tested.getMessage());
+		final SuppressConstructorDemo tested = new SuppressConstructorDemo("a message");
+		assertNull("Message should have been null since we're skipping the execution of the constructor code.", tested.getMessage());
 	}
 
 	/**
@@ -88,8 +66,7 @@ public class SuppressConstructorDemoTest {
 	@Test
 	public void testPartialMockingAndSuppressParentConstructor() throws Exception {
 		suppressConstructorCode(SuppressConstructorSubclassDemo.class);
-		final SuppressConstructorDemo tested = createPartialMock(
-				SuppressConstructorDemo.class, "returnAMessage");
+		final SuppressConstructorDemo tested = createPartialMock(SuppressConstructorDemo.class, "returnAMessage");
 		final String expected = "Hello world!";
 		expectPrivate(tested, "returnAMessage").andReturn(expected);
 		replay(tested);
@@ -100,8 +77,6 @@ public class SuppressConstructorDemoTest {
 
 		assertEquals(expected, actual);
 
-		assertNull(
-				"Message should have been null since we're skipping the execution of the constructor code.",
-				tested.getMessage());
+		assertNull("Message should have been null since we're skipping the execution of the constructor code.", tested.getMessage());
 	}
 }
