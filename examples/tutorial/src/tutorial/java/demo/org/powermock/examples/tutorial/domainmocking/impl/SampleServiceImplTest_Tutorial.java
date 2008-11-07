48eaaaaf9e06cddc738b01d3e69772d1e1c93c33
johan.haleby <johan.haleby@99b96964-f955-0410-9471-33fff0e6aa07>
2008-11-07 09:32:50 +0000

    Updated the tutorial projects with TODO's. It should now be much easier to understand what shall be done.

--- examples/tutorial/src/tutorial/java/demo/org/powermock/examples/tutorial/domainmocking/impl/SampleServiceImplTest_Tutorial.java
+++ examples/tutorial/src/tutorial/java/demo/org/powermock/examples/tutorial/domainmocking/impl/SampleServiceImplTest_Tutorial.java
@@ -23,25 +23,56 @@ import demo.org.powermock.examples.tutorial.domainmocking.domain.BusinessMessage
 import demo.org.powermock.examples.tutorial.domainmocking.domain.Person;
 import demo.org.powermock.examples.tutorial.domainmocking.domain.SampleServiceException;
 
+/**
+ * The purpose of this test is to get 100% coverage of the
+ * {@link SampleServiceImpl} class without any code changes to this class. To
+ * achieve this you need learn how to mock instantiation of domain objects.
+ */
 public class SampleServiceImplTest_Tutorial {
+
+	private SampleServiceImpl tested;
+	private PersonService personServiceMock;
+	private EventService eventService;
 
 	@Before
 	public void setUp() {
+		// TODO Create a mock object of the PersonService and Event service class
+		// TODO Create a new instance of SampleServiceImpl and pass in the created mock objects to the constructor
 	}
 
 	@After
 	public void tearDown() {
+		// TODO Set all references to null
 	}
 
 	@Test
 	public void testCreatePerson() throws Exception {
+        // TODO	Create a mock object of the BusinessMessages class and mock the a construction of "new BusinessMessages" and instead return the mock
+		// TODO Create a mock object of the Person class and mock the a construction of "new Person" and instead return the mock
+		// TODO Expect the call to personService.createPerson(..)
+		// TODO Expect the call to businessMessages.hasErrors(..) and make it return false
+		// TODO Replay all mock objects used and also replay the classes whose constructions were mocked
+		// TODO Perform the actual test and assert that the result is false
+		// TODO Verify all mock objects used and also replay the classes whose constructions were mocked
 	}
 
 	@Test
 	public void testCreatePerson_error() throws Exception {
+		// TODO	Create a mock object of the BusinessMessages class and mock the a construction of "new BusinessMessages" and instead return the mock
+		// TODO Create a mock object of the Person class and mock the a construction of "new Person" and instead return the mock
+		// TODO Expect the call to personService.createPerson(..)
+		// TODO Expect the call to businessMessages.hasErrors(..) and make it return true
+		// TODO Expect the all to eventService.sendErrorEvent(..)
+		// TODO Replay all mock objects used and also replay the classes whose constructions were mocked
+		// TODO Perform the actual test and assert that the result is false
+		// TODO Verify all mock objects used and also replay the classes whose constructions were mocked
 	}
 
 	@Test(expected = SampleServiceException.class)
 	public void testCreatePerson_illegalName() throws Exception {
+		// TODO Create a mock object of the Person class and mock the a construction of "new Person" but instead throw an IllegalArgumentException
+		// TODO Replay all mock objects used and also replay the classes whose constructions were mocked
+		// TODO Perform the actual test and assert that the result is false
+		// TODO Verify all mock objects used and also replay the classes whose constructions were mocked
 	}
 }
