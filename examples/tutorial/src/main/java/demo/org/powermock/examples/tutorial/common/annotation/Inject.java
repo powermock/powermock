398a1b92d8a0d2b100c878b795d2128fc12304a9
johan.haleby <johan.haleby@99b96964-f955-0410-9471-33fff0e6aa07>
2008-11-12 08:52:32 +0000

    Updated javadoc

--- examples/tutorial/src/main/java/demo/org/powermock/examples/tutorial/common/annotation/Inject.java
+++ examples/tutorial/src/main/java/demo/org/powermock/examples/tutorial/common/annotation/Inject.java
@@ -22,11 +22,11 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * A custom marker annotation to demonstrate how use PowerMock to set field
+ * A custom marker annotation to demonstrate how to use PowerMock to set field
  * dependencies. In a real application these dependencies are set by an external
  * dependency injection framework using reflection.
  */
-@Target({ElementType.FIELD, ElementType.CONSTRUCTOR})
+@Target( { ElementType.FIELD, ElementType.CONSTRUCTOR })
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface Inject {
