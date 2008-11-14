de24171e2acf9f5f0cda0f8f490bcbfff55d003d
johan.haleby <johan.haleby@99b96964-f955-0410-9471-33fff0e6aa07>
2008-11-14 14:15:15 +0000

    Fix javadoc for PrepareForTest and PrepareOnlyThisForTest annotations

--- core/src/main/java/org/powermock/core/classloader/annotations/PrepareForTest.java
+++ core/src/main/java/org/powermock/core/classloader/annotations/PrepareForTest.java
@@ -29,20 +29,21 @@ import org.powermock.core.IndicateReloadClass;
  * Classes needed to be defined using this annotation are typically those that
  * needs to be byte-code manipulated. This includes final classes, classes with
  * final, private, static or native methods that should be mocked and also
- * classes that should be return a mock object upon instantiation. To remove
- * static initializers the fully-qualified name(s) to the class(es) must be
- * submitted using the {@link #fullyQualifiedNames()} argument. This is because
- * otherwise the initializer will be executed when defining the class here.
+ * classes that should be return a mock object upon instantiation.
  * <p>
  * This annotation can be placed at both test classes and individual test
  * methods. If placed on a class all test methods in this test class will be
  * handled by PowerMock (to allow for testability). To override this behavior
- * for a single method just place a <code>&#064;PrepareForTest</code>
- * annotation on the specific test method. This is useful in situations where
- * for example you'd like to suppress a static initializer for class A in test
- * method A but in test method B you want the static initializer to be executed.
- * In situations like this you place a <code>&#064;PrepareForTest</code> on
- * method B and exclude class A from the {@link #fullyQualifiedNames()} list.
+ * for a single method just place a <code>&#064;PrepareForTest</code> annotation
+ * on the specific test method. This is useful in situations where for example
+ * you'd like to modify class X in test method A but in test method B you want X
+ * to be left intact. In situations like this you place a
+ * <code>&#064;PrepareForTest</code> on method B and exclude class X from the
+ * {@link #value()} list.
+ * <p>
+ * Sometimes you need to prepare inner classes for testing, this can be done by
+ * suppling the fully-qualified name of the inner-class that should be mocked to
+ * the {@link #fullyQualifiedNames()} list.
  * 
  * <p>
  * The annotation should always be combined with the
