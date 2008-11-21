271eccce64b235e979a0683c8d339cd74458cff8
johan.haleby <johan.haleby@99b96964-f955-0410-9471-33fff0e6aa07>
2008-11-21 07:57:26 +0000

    Added swing demo

---
+++ tests/utils/src/main/java/samples/swing/ReallySimpleSwingDemo.java
@@ -0,0 +1,10 @@
+package samples.swing;
+
+import javax.swing.JOptionPane;
+
+public class ReallySimpleSwingDemo {
+
+	public void displayMessage(String message) {
+		JOptionPane.showMessageDialog(null, message);
+	}
+}
