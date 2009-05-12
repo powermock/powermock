b785cb9ec5e05604d7c0bc05116666e0f301b997
johan.haleby <johan.haleby@99b96964-f955-0410-9471-33fff0e6aa07>
2009-05-12 10:44:10 +0000

    Simple mock policy example

---
+++ modules/module-test/powermock/junit4-test/src/test/java/samples/mockpolicy/MockPolicyUsageExampleTest.java
@@ -0,0 +1,39 @@
+package samples.mockpolicy;
+
+import static org.junit.Assert.assertEquals;
+
+import java.lang.reflect.Method;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.MockPolicy;
+import org.powermock.core.spi.PowerMockPolicy;
+import org.powermock.mockpolicies.MockPolicyClassLoadingSettings;
+import org.powermock.mockpolicies.MockPolicyInterceptionSettings;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
+
+/**
+ * A simple example of a mock policy that stubs out a method call.
+ */
+@RunWith(PowerMockRunner.class)
+@MockPolicy(MockPolicyExample.class)
+public class MockPolicyUsageExampleTest {
+
+	@Test
+	public void exampleOfStubbingOutCallsInParentClass() throws Exception {
+		SomeClassWithAMethod tested = new SomeClassWithAMethod();
+		assertEquals(8.0d, tested.getResult(), 0.0d);
+	}
+}
+
+class MockPolicyExample implements PowerMockPolicy {
+	public void applyClassLoadingPolicy(MockPolicyClassLoadingSettings settings) {
+		settings.addFullyQualifiedNamesOfClassesToLoadByMockClassloader(ResultCalculator.class.getName());
+	}
+
+	public void applyInterceptionPolicy(MockPolicyInterceptionSettings settings) {
+		Method calculateMethod = Whitebox.getMethod(ResultCalculator.class);
+		settings.addSubtituteReturnValue(calculateMethod, 4);
+	}
+}
