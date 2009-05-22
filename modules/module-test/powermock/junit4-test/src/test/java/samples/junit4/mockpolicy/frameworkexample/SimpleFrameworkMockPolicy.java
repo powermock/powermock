83520e6b9ada8e9a5309eaf612d2309ea8b5c597
johan.haleby <johan.haleby@99b96964-f955-0410-9471-33fff0e6aa07>
2009-05-22 12:57:09 +0000

    Added the simple framework + mockpolicy + test

---
+++ modules/module-test/powermock/junit4-test/src/test/java/samples/junit4/mockpolicy/frameworkexample/SimpleFrameworkMockPolicy.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package samples.junit4.mockpolicy.frameworkexample;
+
+import java.lang.reflect.Method;
+
+import org.powermock.core.spi.PowerMockPolicy;
+import org.powermock.mockpolicies.MockPolicyClassLoadingSettings;
+import org.powermock.mockpolicies.MockPolicyInterceptionSettings;
+import org.powermock.reflect.Whitebox;
+
+import samples.mockpolicy.frameworkexample.NativeResult;
+import samples.mockpolicy.frameworkexample.SimpleFramework;
+
+public class SimpleFrameworkMockPolicy implements PowerMockPolicy {
+
+	public static final String NATIVE_RESULT_VALUE = "result";
+
+	public void applyClassLoadingPolicy(MockPolicyClassLoadingSettings settings) {
+		settings.addStaticInitializersToSuppress("samples.mockpolicy.frameworkexample.SimpleFramework");
+	}
+
+	public void applyInterceptionPolicy(MockPolicyInterceptionSettings settings) {
+		final Method doNativeStuffMethod = Whitebox.getMethod(SimpleFramework.class, String.class);
+		NativeResult nativeResult = new NativeResult(NATIVE_RESULT_VALUE);
+		settings.addSubtituteReturnValue(doNativeStuffMethod, nativeResult);
+	}
+}
---
+++ modules/module-test/powermock/junit4-test/src/test/java/samples/junit4/mockpolicy/frameworkexample/SimpleFrameworkUserTest.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package samples.junit4.mockpolicy.frameworkexample;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.MockPolicy;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import samples.mockpolicy.frameworkexample.SimpleFrameworkUser;
+
+@RunWith(PowerMockRunner.class)
+@MockPolicy(SimpleFrameworkMockPolicy.class)
+public class SimpleFrameworkUserTest {
+
+	@Test
+	public void testPerformComplexOperation() throws Exception {
+		SimpleFrameworkUser tested = new SimpleFrameworkUser();
+		assertEquals(SimpleFrameworkMockPolicy.NATIVE_RESULT_VALUE, tested.performComplexOperation("some complex stuff"));
+	}
+
+}
