d679d8b0e0a7d8c14f1ec880148aa23d19b0504b
johan.haleby <johan.haleby@99b96964-f955-0410-9471-33fff0e6aa07>
2009-07-31 13:54:48 +0000

    removed unnessecary null check

--- modules/module-impl/junit4/src/main/java/org/powermock/modules/junit4/internal/impl/testcaseworkaround/PowerMockJUnit4MethodValidator.java
+++ modules/module-impl/junit4/src/main/java/org/powermock/modules/junit4/internal/impl/testcaseworkaround/PowerMockJUnit4MethodValidator.java
@@ -40,77 +40,77 @@ import org.powermock.reflect.Whitebox;
 @SuppressWarnings("deprecation")
 public class PowerMockJUnit4MethodValidator extends MethodValidator {
 
-	public PowerMockJUnit4MethodValidator(TestClass testClass) {
-		super(testClass);
-	}
+    public PowerMockJUnit4MethodValidator(TestClass testClass) {
+        super(testClass);
+    }
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public void validateInstanceMethods() {
-		validateTestMethods(After.class, false);
-		validateTestMethods(Before.class, false);
-		validateTestMethods(Test.class, false);
+    @SuppressWarnings("unchecked")
+    @Override
+    public void validateInstanceMethods() {
+        validateTestMethods(After.class, false);
+        validateTestMethods(Before.class, false);
+        validateTestMethods(Test.class, false);
 
-		TestClass testClass = (TestClass) Whitebox.getInternalState(this, "fTestClass", MethodValidator.class);
-		Class<?> classUnderTest = (Class<?>) Whitebox.getInternalState(testClass, "fClass");
-		List<Throwable> fErrors = (List<Throwable>) Whitebox.getInternalState(this, "fErrors", MethodValidator.class);
+        TestClass testClass = (TestClass) Whitebox.getInternalState(this, "fTestClass", MethodValidator.class);
+        Class<?> classUnderTest = (Class<?>) Whitebox.getInternalState(testClass, "fClass");
+        List<Throwable> fErrors = (List<Throwable>) Whitebox.getInternalState(this, "fErrors", MethodValidator.class);
 
-		List<Method> methods = getTestMethods(testClass, classUnderTest);
-		if (methods.size() == 0)
-			fErrors.add(new Exception("No runnable methods"));
-	}
+        List<Method> methods = getTestMethods(testClass, classUnderTest);
+        if (methods.size() == 0)
+            fErrors.add(new Exception("No runnable methods"));
+    }
 
-	private List<Method> getTestMethods(TestClass testClass, Class<?> classUnderTest) {
-		List<Method> methods = testClass.getAnnotatedMethods(Test.class);
-		if (methods.isEmpty()) {
-			methods.addAll(getTestMethodsWithNoAnnotation(classUnderTest));
-		}
-		return methods;
-	}
+    private List<Method> getTestMethods(TestClass testClass, Class<?> classUnderTest) {
+        List<Method> methods = testClass.getAnnotatedMethods(Test.class);
+        if (methods.isEmpty()) {
+            methods.addAll(getTestMethodsWithNoAnnotation(classUnderTest));
+        }
+        return methods;
+    }
 
-	/**
-	 * This is a rip-off of the
-	 * {@link MethodValidator#validateInstanceMethods()} with the exception that
-	 * this method also searches for test methods if the class extends
-	 * {@link TestCase} and has methods that starts with test which are not
-	 * annotated.
-	 */
-	@SuppressWarnings("unchecked")
-	private void validateTestMethods(Class<? extends Annotation> annotation, boolean isStatic) {
-		TestClass testClass = (TestClass) Whitebox.getInternalState(this, "fTestClass", MethodValidator.class);
-		Class<?> classUnderTest = (Class<?>) Whitebox.getInternalState(testClass, "fClass");
-		List<Method> methods = null;
-		if ((methods == null || methods.isEmpty()) && TestCase.class.equals(classUnderTest.getSuperclass()) && !isStatic) {
-			methods = getTestMethodsWithNoAnnotation(classUnderTest);
-		} else {
-			methods = testClass.getAnnotatedMethods(annotation);
-		}
+    /**
+     * This is a rip-off of the
+     * {@link MethodValidator#validateInstanceMethods()} with the exception that
+     * this method also searches for test methods if the class extends
+     * {@link TestCase} and has methods that starts with test which are not
+     * annotated.
+     */
+    @SuppressWarnings("unchecked")
+    private void validateTestMethods(Class<? extends Annotation> annotation, boolean isStatic) {
+        TestClass testClass = (TestClass) Whitebox.getInternalState(this, "fTestClass", MethodValidator.class);
+        Class<?> classUnderTest = (Class<?>) Whitebox.getInternalState(testClass, "fClass");
+        final List<Method> methods;
+        if (TestCase.class.equals(classUnderTest.getSuperclass()) && !isStatic) {
+            methods = getTestMethodsWithNoAnnotation(classUnderTest);
+        } else {
+            methods = testClass.getAnnotatedMethods(annotation);
+        }
 
-		List<Throwable> fErrors = (List<Throwable>) Whitebox.getInternalState(this, "fErrors", MethodValidator.class);
-		for (Method each : methods) {
-			if (Modifier.isStatic(each.getModifiers()) != isStatic) {
-				String state = isStatic ? "should" : "should not";
-				fErrors.add(new Exception("Method " + each.getName() + "() " + state + " be static"));
-			}
-			if (!Modifier.isPublic(each.getDeclaringClass().getModifiers()))
-				fErrors.add(new Exception("Class " + each.getDeclaringClass().getName() + " should be public"));
-			if (!Modifier.isPublic(each.getModifiers()))
-				fErrors.add(new Exception("Method " + each.getName() + " should be public"));
-			if (each.getReturnType() != Void.TYPE)
-				fErrors.add(new Exception("Method " + each.getName() + " should be void"));
-			if (each.getParameterTypes().length != 0)
-				fErrors.add(new Exception("Method " + each.getName() + " should have no parameters"));
-		}
-	}
+        List<Throwable> fErrors = (List<Throwable>) Whitebox.getInternalState(this, "fErrors", MethodValidator.class);
+        for (Method each : methods) {
+            if (Modifier.isStatic(each.getModifiers()) != isStatic) {
+                String state = isStatic ? "should" : "should not";
+                fErrors.add(new Exception("Method " + each.getName() + "() " + state + " be static"));
+            }
+            if (!Modifier.isPublic(each.getDeclaringClass().getModifiers()))
+                fErrors.add(new Exception("Class " + each.getDeclaringClass().getName() + " should be public"));
+            if (!Modifier.isPublic(each.getModifiers()))
+                fErrors.add(new Exception("Method " + each.getName() + " should be public"));
+            if (each.getReturnType() != Void.TYPE)
+                fErrors.add(new Exception("Method " + each.getName() + " should be void"));
+            if (each.getParameterTypes().length != 0)
+                fErrors.add(new Exception("Method " + each.getName() + " should have no parameters"));
+        }
+    }
 
-	private List<Method> getTestMethodsWithNoAnnotation(Class<?> testClass) {
-		List<Method> potentialTestMethods = new LinkedList<Method>();
-		Method[] methods = testClass.getMethods();
-		for (Method method : methods) {
-			if (method.getName().startsWith("test")) {
-				potentialTestMethods.add(method);
-			}
-		}
-		return potentialTestMethods;
-	}
+    private List<Method> getTestMethodsWithNoAnnotation(Class<?> testClass) {
+        List<Method> potentialTestMethods = new LinkedList<Method>();
+        Method[] methods = testClass.getMethods();
+        for (Method method : methods) {
+            if (method.getName().startsWith("test")) {
+                potentialTestMethods.add(method);
+            }
+        }
+        return potentialTestMethods;
+    }
 }
