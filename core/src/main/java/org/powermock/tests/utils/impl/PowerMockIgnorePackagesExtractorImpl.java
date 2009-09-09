029432bd1d21082e766d1aa51b140953a1f64054
johan.haleby <johan.haleby@99b96964-f955-0410-9471-33fff0e6aa07>
2009-09-09 06:43:15 +0000

    Packages ignored for test in parent test classes are now automatically ignored as well when using @PowerMockIgnore

--- core/src/main/java/org/powermock/tests/utils/impl/PowerMockIgnorePackagesExtractorImpl.java
+++ core/src/main/java/org/powermock/tests/utils/impl/PowerMockIgnorePackagesExtractorImpl.java
@@ -1,18 +1,34 @@
 package org.powermock.tests.utils.impl;
 
 import java.lang.reflect.AnnotatedElement;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.tests.utils.IgnorePackagesExtractor;
 
 public class PowerMockIgnorePackagesExtractorImpl implements IgnorePackagesExtractor {
 
-	public String[] getPackagesToIgnore(AnnotatedElement element) {
-		PowerMockIgnore annotation = element.getAnnotation(PowerMockIgnore.class);
-		if (annotation != null) {
-			return annotation.value();
-		}
-		return new String[0];
-	}
+    public String[] getPackagesToIgnore(AnnotatedElement element) {
+        List<String> ignoredPackages = new LinkedList<String>();
+        PowerMockIgnore annotation = element.getAnnotation(PowerMockIgnore.class);
+        if (annotation != null) {
+            String[] ignores = annotation.value();
+            for (String ignorePackage : ignores) {
+                ignoredPackages.add(ignorePackage);
+            }
+        }
+        if (element instanceof Class<?>) {
+            Class<?> klazz = (Class<?>) element;
+            Class<?> superclass = klazz.getSuperclass();
+            if (superclass != null && !superclass.equals(Object.class)) {
+                String[] packagesToIgnore = getPackagesToIgnore(superclass);
+                for (String packageToIgnore : packagesToIgnore) {
+                    ignoredPackages.add(packageToIgnore);
+                }
+            }
+        }
+        return ignoredPackages.toArray(new String[ignoredPackages.size()]);
+    }
 
 }
---
+++ core/src/test/java/org/powermock/tests/utils/impl/PowerMockIgnorePackagesExtractorImplTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.powermock.tests.utils.impl;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+
+public class PowerMockIgnorePackagesExtractorImplTest {
+
+    /**
+     * Makes sure that issue <a
+     * href="http://code.google.com/p/powermock/issues/detail?id=150">150</a> is
+     * solved.
+     */
+    @Test
+    public void shouldFindIgnorePackagesInTheWholeClassHierarchy() throws Exception {
+        final PowerMockIgnorePackagesExtractorImpl tested = new PowerMockIgnorePackagesExtractorImpl();
+        final String[] packagesToIgnore = tested.getPackagesToIgnore(IgnoreAnnotatedDemoClass.class);
+        assertEquals(4, packagesToIgnore.length);
+        assertEquals("ignore0", packagesToIgnore[0]);
+        assertEquals("ignore1", packagesToIgnore[1]);
+        assertEquals("ignore2", packagesToIgnore[2]);
+        assertEquals("ignore3", packagesToIgnore[3]);
+    }
+
+    @PowerMockIgnore( { "ignore0", "ignore1" })
+    private class IgnoreAnnotatedDemoClass extends IgnoreAnnotatedDemoClassParent {
+
+    }
+
+    @PowerMockIgnore("ignore2")
+    private class IgnoreAnnotatedDemoClassParent extends IgnoreAnnotatedDemoClassGrandParent {
+
+    }
+
+    @PowerMockIgnore("ignore3")
+    private class IgnoreAnnotatedDemoClassGrandParent {
+
+    }
+}
