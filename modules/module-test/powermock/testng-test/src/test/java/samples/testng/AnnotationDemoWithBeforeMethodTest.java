39f6b74902e58b377cdc0f8f77d023342d996bdf
johan.haleby <johan.haleby@99b96964-f955-0410-9471-33fff0e6aa07>
2009-09-11 19:07:52 +0000

    Added testng before defect

--- modules/module-test/powermock/testng-test/src/test/java/samples/testng/AnnotationDemoWithBeforeMethodTest.java
+++ modules/module-test/powermock/testng-test/src/test/java/samples/testng/AnnotationDemoWithBeforeMethodTest.java
@@ -6,24 +6,30 @@ import static org.powermock.api.easymock.PowerMock.verifyAll;
 
 import org.powermock.api.easymock.annotation.Mock;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import samples.Service;
 import samples.annotationbased.AnnotationDemo;
 
 /**
- * Verifies that PowerMock test listeners works correctly in TestNG.
+ * Verifies that PowerMock test listeners works correctly with before methods in
+ * TestNG.
  */
-public class AnnotationDemoTest {
+public class AnnotationDemoWithBeforeMethodTest {
 
     @Mock
     private Service serviceMock;
 
     private AnnotationDemo tested;
 
+    @BeforeMethod
+    public void setup() {
+        tested = new AnnotationDemo(serviceMock);
+    }
+
     @Test
     public void assertInjectionWorked() throws Exception {
-        tested = new AnnotationDemo(serviceMock);
         final String expected = "mock";
         expect(serviceMock.getServiceMessage()).andReturn(expected);
 
