53b121312b4a24b3aae86e141e1b1a0c73901fb5
johan.haleby <johan.haleby@99b96964-f955-0410-9471-33fff0e6aa07>
2009-09-21 11:42:53 +0000

    Added a test that proves a hashcode problem when mocking instance methods in final system classes

---
+++ modules/module-test/powermock/junit4-test/src/test/java/samples/junit4/system/FieldMockDefect.java
@@ -0,0 +1,46 @@
+package samples.junit4.system;
+
+import static org.easymock.EasyMock.expect;
+import static org.junit.Assert.assertEquals;
+import static org.powermock.api.easymock.PowerMock.createMock;
+import static org.powermock.api.easymock.PowerMock.replayAll;
+import static org.powermock.api.easymock.PowerMock.verifyAll;
+
+import java.lang.reflect.Field;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+/**
+ * 
+ * The reason why it fails is because we create a new instance of the field
+ * using Whitebox but then hashCode doesn't work because it's missing
+ * constructor args.
+ * <p>
+ * 
+ * if (Field.class.isAssignableFrom(mock.getClass())) {
+ * Whitebox.setInternalState(mock, "clazz", (Object) Object.class);
+ * Whitebox.setInternalState(mock, "name", Object.class.getName()); }
+ * 
+ */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(Field.class)
+public class FieldMockDefect {
+    @Test
+    public void test() {
+        final Field fieldMock = createMock(Field.class);
+        expect(fieldMock.getName()).andReturn("foo");
+        replayAll();
+        assertEquals("foo", new ClassUnderTest().foo(fieldMock));
+        verifyAll();
+
+    }
+
+    private static final class ClassUnderTest {
+        public String foo(Field field) {
+            return field.getName();
+        }
+    }
+}
