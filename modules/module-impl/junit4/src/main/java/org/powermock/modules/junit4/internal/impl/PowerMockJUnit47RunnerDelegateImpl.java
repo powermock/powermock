cfacae07a4849c0907cd99d070fc904a37567be1
johan.haleby <johan.haleby@99b96964-f955-0410-9471-33fff0e6aa07>
2009-10-12 09:05:11 +0000

    Fixed a bug in junit 4.7 rules, but execeptions doesn't yet work

--- modules/module-impl/junit4/src/main/java/org/powermock/modules/junit4/internal/impl/PowerMockJUnit47RunnerDelegateImpl.java
+++ modules/module-impl/junit4/src/main/java/org/powermock/modules/junit4/internal/impl/PowerMockJUnit47RunnerDelegateImpl.java
@@ -21,56 +21,85 @@ import org.powermock.reflect.Whitebox;
  */
 @SuppressWarnings("deprecation")
 public class PowerMockJUnit47RunnerDelegateImpl extends PowerMockJUnit44RunnerDelegateImpl {
-	public PowerMockJUnit47RunnerDelegateImpl(Class<?> klass, String[] methodsToRun, PowerMockTestListener[] listeners) throws InitializationError {
-		super(klass, methodsToRun, listeners);
-	}
+    public boolean hasRules;
 
-	public PowerMockJUnit47RunnerDelegateImpl(Class<?> klass, String[] methodsToRun) throws InitializationError {
-		super(klass, methodsToRun);
-	}
+    public PowerMockJUnit47RunnerDelegateImpl(Class<?> klass, String[] methodsToRun, PowerMockTestListener[] listeners) throws InitializationError {
+        super(klass, methodsToRun, listeners);
+    }
 
-	public PowerMockJUnit47RunnerDelegateImpl(Class<?> klass) throws InitializationError {
-		super(klass);
-	}
+    public PowerMockJUnit47RunnerDelegateImpl(Class<?> klass, String[] methodsToRun) throws InitializationError {
+        super(klass, methodsToRun);
+    }
 
-	@Override
-	protected PowerMockJUnit44MethodRunner createPowerMockRunner(final Object testInstance, final TestMethod testMethod, RunNotifier notifier,
-			Description description, final boolean extendsFromTestCase) {
-		return new PowerMockJUnit47MethodRunner(testInstance, testMethod, notifier, description, extendsFromTestCase);
-	}
+    public PowerMockJUnit47RunnerDelegateImpl(Class<?> klass) throws InitializationError {
+        super(klass);
+    }
 
-	protected class PowerMockJUnit47MethodRunner extends PowerMockJUnit44MethodRunner {
+    @Override
+    protected PowerMockJUnit44MethodRunner createPowerMockRunner(final Object testInstance, final TestMethod testMethod, RunNotifier notifier,
+            Description description, final boolean extendsFromTestCase) {
+        return new PowerMockJUnit47MethodRunner(testInstance, testMethod, notifier, description, extendsFromTestCase);
+    }
 
-		protected PowerMockJUnit47MethodRunner(Object testInstance, TestMethod method, RunNotifier notifier, Description description,
-				boolean extendsFromTestCase) {
-			super(testInstance, method, notifier, description, extendsFromTestCase);
-		}
+    protected class PowerMockJUnit47MethodRunner extends PowerMockJUnit44MethodRunner {
 
-		@Override
-		public void executeTest(final Method method, final Object testInstance, final Runnable test) {
-			Set<Field> rules = Whitebox.getFieldsAnnotatedWith(testInstance, Rule.class);
-			if (rules.isEmpty()) {
-				executeTestInSuper(method, testInstance, test);
-			} else {
-				for (Field field : rules) {
-					try {
-						MethodRule rule = (MethodRule) field.get(testInstance);
-						rule.apply(new Statement() {
+        private final class LastRuleTestExecutorStatement extends Statement {
+            private final Runnable test;
+            private final Object testInstance;
+            private final Method method;
+            private final int noOfRules;
+            private final int currentRule;
 
-							@Override
-							public void evaluate() throws Throwable {
-								executeTestInSuper(method, testInstance, test);
-							}
-						}, new FrameworkMethod(method), testInstance);
-					} catch (Exception e) {
-						throw new RuntimeException(e);
-					}
-				}
-			}
-		}
+            private LastRuleTestExecutorStatement(int currentRuleNumber, int noOfRules, Runnable test, Object testInstance, Method method) {
+                this.currentRule = currentRuleNumber;
+                this.noOfRules = noOfRules;
+                this.test = test;
+                this.testInstance = testInstance;
+                this.method = method;
+            }
 
-		private void executeTestInSuper(final Method method, final Object testInstance, final Runnable test) {
-			super.executeTest(method, testInstance, test);
-		}
-	}
+            @Override
+            public void evaluate() throws Throwable {
+                if (currentRule == noOfRules) {
+                    executeTestInSuper(method, testInstance, test);
+                }
+            }
+        }
+
+        protected PowerMockJUnit47MethodRunner(Object testInstance, TestMethod method, RunNotifier notifier, Description description,
+                boolean extendsFromTestCase) {
+            super(testInstance, method, notifier, description, extendsFromTestCase);
+        }
+
+        @Override
+        public void executeTest(final Method method, final Object testInstance, final Runnable test) {
+            final Set<Field> rules = Whitebox.getFieldsAnnotatedWith(testInstance, Rule.class);
+            hasRules = !rules.isEmpty();
+            if (hasRules) {
+                executeTestInSuper(method, testInstance, test);
+            } else {
+                int processedFields = 0;
+                for (Field field : rules) {
+                    processedFields++;
+                    try {
+                        MethodRule rule = (MethodRule) field.get(testInstance);
+                        Statement statement = rule.apply(
+                                new LastRuleTestExecutorStatement(processedFields, rules.size(), test, testInstance, method), new FrameworkMethod(
+                                        method), testInstance);
+                        statement.evaluate();
+                    } catch (RuntimeException e) {
+                        throw (RuntimeException) e;
+                    } catch (AssertionError e) {
+                        throw e;
+                    } catch (Throwable e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
+
+        private void executeTestInSuper(final Method method, final Object testInstance, final Runnable test) {
+                super.executeTest(method, testInstance, test);
+        }
+    }
 }
