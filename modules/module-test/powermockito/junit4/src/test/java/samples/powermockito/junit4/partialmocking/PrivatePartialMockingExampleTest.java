d9e3572c9f3991c71f807cda040a3f4c1c54988f
johan.haleby <johan.haleby@99b96964-f955-0410-9471-33fff0e6aa07>
2009-10-28 15:06:43 +0000

    Added more mock examples for PowerMockito

---
+++ modules/module-test/powermockito/junit4/src/test/java/samples/powermockito/junit4/partialmocking/PrivatePartialMockingExampleTest.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package samples.powermockito.junit4.partialmocking;
+
+import static org.junit.Assert.assertEquals;
+import static org.powermock.api.mockito.PowerMockito.spy;
+import static org.powermock.api.mockito.PowerMockito.verifyPrivate;
+import static org.powermock.api.mockito.PowerMockito.when;
+import static org.powermock.api.support.membermodification.MemberMatcher.method;
+
+import java.lang.reflect.Method;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import samples.partialmocking.PrivatePartialMockingExample;
+
+/**
+ * Asserts that partial mocking (spying) with PowerMockito works for non-final
+ * private methods.
+ */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(PrivatePartialMockingExample.class)
+public class PrivatePartialMockingExampleTest {
+
+    @Test
+    public void spyingOnPrivateMethodsWorks() throws Exception {
+        final String expected = "TEST VALUE";
+        PrivatePartialMockingExample underTest = spy(new PrivatePartialMockingExample());
+        final String nameOfMethodToMock = "methodToMock";
+        final String input = "input";
+        when(underTest, nameOfMethodToMock, input).thenReturn(expected);
+
+        assertEquals(expected, underTest.methodToTest());
+
+        verifyPrivate(underTest).invoke(nameOfMethodToMock, input);
+    }
+
+    @Test
+    public void spyingOnPrivateMethodsWorksWithoutSpecifyingMethodName() throws Exception {
+        final String expected = "TEST VALUE";
+        PrivatePartialMockingExample underTest = spy(new PrivatePartialMockingExample());
+        final String input = "input";
+        final Method methodToMock = method(PrivatePartialMockingExample.class, String.class);
+        when(underTest, methodToMock).withArguments(input).thenReturn(expected);
+
+        assertEquals(expected, underTest.methodToTest());
+
+        verifyPrivate(underTest).invoke(methodToMock).withArguments(input);
+    }
+}
