68f1d1d6af88e11e1d308db8e0c9cb6741f3fdbe
johan.haleby <johan.haleby@99b96964-f955-0410-9471-33fff0e6aa07>
2009-10-28 15:36:00 +0000

    Added new file example for mockito

---
+++ modules/module-test/powermockito/junit4/src/test/java/samples/powermockito/junit4/whennew/NewFileExampleTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package samples.powermockito.junit4.whennew;
+
+import static org.junit.Assert.assertTrue;
+import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.powermock.api.mockito.PowerMockito.verifyNew;
+import static org.powermock.api.mockito.PowerMockito.when;
+import static org.powermock.api.mockito.PowerMockito.whenNew;
+
+import java.io.File;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import samples.expectnew.NewFileExample;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(NewFileExample.class)
+public class NewFileExampleTest {
+    @Test
+    public void assertThatMockingFileWorks() throws Exception {
+        final String directoryPath = "mocked path";
+
+        File directoryMock = mock(File.class);
+
+        whenNew(File.class).withArguments(directoryPath).thenReturn(directoryMock);
+        when(directoryMock.exists()).thenReturn(false);
+        when(directoryMock.mkdirs()).thenReturn(true);
+
+        assertTrue(new NewFileExample().createDirectoryStructure(directoryPath));
+
+        verifyNew(File.class).withArguments(directoryPath);
+    }
+}
