dc30c083d64a2ac02aa09c4af12f25eca6f19321
jan.kronquist <jan.kronquist@99b96964-f955-0410-9471-33fff0e6aa07>
2010-02-18 20:06:34 +0000

    DeepCloner keeps state and PowerMockRule reuse ClassloaderExecutor

---
+++ api/support/src/main/java/org/powermock/api/support/DoNotClone.java
@@ -0,0 +1,13 @@
+package org.powermock.api.support;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.FIELD})
+@Inherited
+public @interface DoNotClone {
+}
