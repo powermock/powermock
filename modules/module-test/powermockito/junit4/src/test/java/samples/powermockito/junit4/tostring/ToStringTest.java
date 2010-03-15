6561ef8798f42837c93c44d807a80386bd653e1e
johan.haleby <johan.haleby@99b96964-f955-0410-9471-33fff0e6aa07>
2010-03-15 17:24:01 +0000

    Fixed so that a 'mock name' is set in the Mockito extension API. Fixes NullPointerException when e.g. toString is invoked on a mock. (issue 239)

---
+++ modules/module-test/powermockito/junit4/src/test/java/samples/powermockito/junit4/tostring/ToStringTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package samples.powermockito.junit4.tostring;
+
+import static org.junit.Assert.assertNotNull;
+
+import javax.crypto.Cipher;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+/**
+ * Test that demonstrates that <a
+ * href="http://code.google.com/p/powermock/issues/detail?id=239">issue 239</a>
+ * is resolved.
+ */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(ToStringTest.class)
+public class ToStringTest {
+    private Cipher cipher;
+
+    @Test
+    public void toStringInvocationWorksInMockito() throws Exception {
+        cipher = PowerMockito.mock(Cipher.class);
+
+        assertNotNull(cipher.toString());
+    }
+}
