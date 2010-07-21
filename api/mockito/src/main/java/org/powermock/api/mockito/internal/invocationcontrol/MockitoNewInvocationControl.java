86e4f8b565d098ce218af120671aaed4d355d6d9
johan.haleby <johan.haleby@99b96964-f955-0410-9471-33fff0e6aa07>
2010-07-21 17:36:27 +0000

    Fixed bug in Whitebox when dealing for member constructors and var args invocations

--- api/mockito/src/main/java/org/powermock/api/mockito/internal/invocationcontrol/MockitoNewInvocationControl.java
+++ api/mockito/src/main/java/org/powermock/api/mockito/internal/invocationcontrol/MockitoNewInvocationControl.java
@@ -29,84 +29,84 @@ import org.powermock.core.spi.support.InvocationSubstitute;
 import org.powermock.reflect.internal.WhiteboxImpl;
 
 public class MockitoNewInvocationControl<T> implements NewInvocationControl<OngoingStubbing<T>> {
-    private final InvocationSubstitute<T> substitute;
+	private final InvocationSubstitute<T> substitute;
 
-    public MockitoNewInvocationControl(InvocationSubstitute<T> substitute) {
-        if (substitute == null) {
-            throw new IllegalArgumentException("Internal error: substitute cannot be null.");
-        }
-        this.substitute = substitute;
-    }
+	public MockitoNewInvocationControl(InvocationSubstitute<T> substitute) {
+		if (substitute == null) {
+			throw new IllegalArgumentException("Internal error: substitute cannot be null.");
+		}
+		this.substitute = substitute;
+	}
 
-    public Object invoke(Class<?> type, Object[] args, Class<?>[] sig) throws Exception {
-        Constructor<?> constructor = WhiteboxImpl.getConstructor(type, sig);
-        if (constructor.isVarArgs()) {
-            /*
-             * Get the first argument because this contains the actual varargs
-             * arguments.
-             */
-            args = (Object[]) args[0];
-        }
-        try {
-            return substitute.performSubstitutionLogic(args);
-        } catch (MockitoAssertionError e) {
-            InvocationControlAssertionError.throwAssertionErrorForNewSubstitutionFailure(e, type);
-        }
+	public Object invoke(Class<?> type, Object[] args, Class<?>[] sig) throws Exception {
+		Constructor<?> constructor = WhiteboxImpl.getConstructor(type, sig);
+		if (constructor.isVarArgs()) {
+			/*
+			 * Get the first argument because this contains the actual varargs
+			 * arguments.
+			 */
+			args = (Object[]) args[args.length - 1];
+		}
+		try {
+			return substitute.performSubstitutionLogic(args);
+		} catch (MockitoAssertionError e) {
+			InvocationControlAssertionError.throwAssertionErrorForNewSubstitutionFailure(e, type);
+		}
 
-        // Won't happen
-        return null;
-    }
+		// Won't happen
+		return null;
+	}
 
-    public OngoingStubbing<T> expectSubstitutionLogic(Object... arguments) throws Exception {
-        return Mockito.when(substitute.performSubstitutionLogic(arguments));
-    }
+	public OngoingStubbing<T> expectSubstitutionLogic(Object... arguments) throws Exception {
+		return Mockito.when(substitute.performSubstitutionLogic(arguments));
+	}
 
-    public InvocationSubstitute<T> getSubstitute() {
-        return substitute;
-    }
+	public InvocationSubstitute<T> getSubstitute() {
+		return substitute;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    public synchronized Object replay(Object... mocks) {
-        return null;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	public synchronized Object replay(Object... mocks) {
+		return null;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    public synchronized Object verify(Object... mocks) {
-        final VerificationMode verificationMode;
-        Object mode = MockRepository.getAdditionalState("VerificationMode");
-        if (mode != null) {
-            if (mode instanceof VerificationMode) {
-                verificationMode = (VerificationMode) mode;
-            } else {
-                throw new IllegalStateException("Internal error. VerificationMode in MockRepository was not of type "
-                        + VerificationMode.class.getName() + ".");
-            }
-        } else {
-            verificationMode = times(1);
-        }
-        Mockito.verify(substitute, verificationMode);
-        return null;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	public synchronized Object verify(Object... mocks) {
+		final VerificationMode verificationMode;
+		Object mode = MockRepository.getAdditionalState("VerificationMode");
+		if (mode != null) {
+			if (mode instanceof VerificationMode) {
+				verificationMode = (VerificationMode) mode;
+			} else {
+				throw new IllegalStateException("Internal error. VerificationMode in MockRepository was not of type "
+						+ VerificationMode.class.getName() + ".");
+			}
+		} else {
+			verificationMode = times(1);
+		}
+		Mockito.verify(substitute, verificationMode);
+		return null;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @SuppressWarnings("unchecked")
-    public synchronized Object reset(Object... mocks) {
-        Mockito.<InvocationSubstitute<T>> reset(substitute);
-        return null;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@SuppressWarnings("unchecked")
+	public synchronized Object reset(Object... mocks) {
+		Mockito.<InvocationSubstitute<T>> reset(substitute);
+		return null;
+	}
 
-    public void verifyNoMoreInteractions() {
-        try {
-            Mockito.verifyNoMoreInteractions(substitute);
-        } catch (MockitoAssertionError e) {
-            InvocationControlAssertionError.updateErrorMessageForVerifyNoMoreInteractions(e);
-            throw e;
-        }
-    }
+	public void verifyNoMoreInteractions() {
+		try {
+			Mockito.verifyNoMoreInteractions(substitute);
+		} catch (MockitoAssertionError e) {
+			InvocationControlAssertionError.updateErrorMessageForVerifyNoMoreInteractions(e);
+			throw e;
+		}
+	}
 }
---
+++ modules/module-test/powermockito/junit4/src/test/java/samples/powermockito/junit4/constructor/InnerConstructorsAreFoundTest.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package samples.powermockito.junit4.constructor;
+
+import static org.powermock.api.mockito.PowerMockito.whenNew;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@PrepareForTest({ InnerConstructorsAreFoundTest.WorkingObjectUnderTesting.class,
+		InnerConstructorsAreFoundTest.BuggyObjectUnderTesting.class,
+		InnerConstructorsAreFoundTest.StillBuggyObjectUnderTesting.class })
+@RunWith(PowerMockRunner.class)
+public class InnerConstructorsAreFoundTest {
+
+	interface AnyInterface {
+	};
+
+	class AnyImplementation implements AnyInterface {
+		@Override
+		public boolean equals(Object obj) {
+			return true;
+		}
+	}
+
+	class SomeOtherImplementationOfSomethingElse {
+		@Override
+		public boolean equals(Object obj) {
+			return true;
+		}
+	};
+
+	// <ObjectsUnderTesting>
+	class WorkingObjectUnderTesting {
+		void methodToTest() {
+			new WorkingObjectToMock(new AnyImplementation());
+		}
+	}
+
+	class BuggyObjectUnderTesting {
+		void methodToTest() {
+			new BuggyObjectToMock(new SomeOtherImplementationOfSomethingElse(), new AnyImplementation());
+		}
+	}
+
+	class StillBuggyObjectUnderTesting {
+		void methodToTest() {
+			new StillBuggyObjectToMock(new SomeOtherImplementationOfSomethingElse(),
+					new AnyInterface[] { new AnyImplementation() });
+		}
+	}
+
+	// </ObjectsUnderTesting>
+
+	// <ObjectsToMock>
+	class WorkingObjectToMock {
+		public WorkingObjectToMock(AnyInterface... anys) {
+		}
+	}
+
+	class BuggyObjectToMock {
+		private final AnyInterface[] anys;
+
+		public BuggyObjectToMock(SomeOtherImplementationOfSomethingElse other, AnyInterface... anys) {
+			this.anys = anys;
+		}
+	}
+
+	class StillBuggyObjectToMock {
+		private final AnyInterface[] anys;
+
+		public StillBuggyObjectToMock(SomeOtherImplementationOfSomethingElse other, AnyInterface[] anys) {
+			this.anys = anys;
+		}
+	}
+
+	@Mock
+	WorkingObjectToMock mockedWorkingObject;
+	@Mock
+	BuggyObjectToMock mockedBuggyObject;
+	@Mock
+	StillBuggyObjectToMock mockedStillBuggyObject;
+
+	@Test
+	public void shouldInjectNewInstance() throws Exception {
+		whenNew(WorkingObjectToMock.class).withArguments(new AnyInterface[] { new AnyImplementation() }).thenReturn(
+				mockedWorkingObject);
+
+		new WorkingObjectUnderTesting().methodToTest();
+	}
+
+	@Test
+	public void shouldInjectNewInstanceButFails() throws Exception {
+		// this is the same as above and throws same exception
+		whenNew(BuggyObjectToMock.class).withArguments(new SomeOtherImplementationOfSomethingElse(),
+				(Object[]) new AnyInterface[] { new AnyImplementation() }).thenReturn(mockedBuggyObject);
+
+		new BuggyObjectUnderTesting().methodToTest();
+	}
+
+	@Test
+	public void shouldInjectNewInstanceButStillFails() throws Exception {
+		whenNew(StillBuggyObjectToMock.class).withArguments(new SomeOtherImplementationOfSomethingElse(),
+				(Object[]) new AnyInterface[] { new AnyImplementation() }).thenReturn(mockedStillBuggyObject);
+
+		new StillBuggyObjectUnderTesting().methodToTest();
+	}
+}
