85d0acc2b5718f5f65c908299f3ec573675d4829
johan.haleby <johan.haleby@99b96964-f955-0410-9471-33fff0e6aa07>
2010-12-26 08:33:20 +0000

    Added a defect example

---
+++ modules/module-test/mockito/junit4/src/test/java/samples/powermockito/junit4/whennew/DoesntSupportCreatingMocksInFieldsWhenNewDefect.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package samples.powermockito.junit4.whennew;
+
+import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.powermock.api.mockito.PowerMockito.whenNew;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import samples.newmocking.NewDemo;
+import samples.newmocking.SomeDependency;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest( { 	NewDemo.class })
+/**
+ * Issue <a href="298">http://code.google.com/p/powermock/issues/detail?id=298</a>
+ */
+public class DoesntSupportCreatingMocksInFieldsWhenNewDefect {
+
+	final SomeDependency somethingUsedByMethodUnderTest=mock(SomeDependency.class); // for some reason the mocking only works if loadingPool is a local variable not a field (like all the other mocks)
+
+	@Test
+	public void methodUnderTestShouldWorkWithClassLevelMock() throws Exception {
+
+		whenNew(SomeDependency.class).withNoArguments().thenReturn(somethingUsedByMethodUnderTest);
+
+		NewDemo objectUnderTest = new NewDemo();
+		
+		objectUnderTest.methodUnderTest();
+		
+	}
+
+
+	
+}
