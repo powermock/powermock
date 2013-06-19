cbc0b022272dbb4e24b1796ba353841a6ca8f92d
johan.haleby@gmail.com <johan.haleby@gmail.com@99b96964-f955-0410-9471-33fff0e6aa07>
2013-06-19 15:52:45 +0000

    Fixed CRLF problems

--- examples/slf4j-mockito/src/main/java/demo/org/powermock/examples/Slf4jUser.java
+++ examples/slf4j-mockito/src/main/java/demo/org/powermock/examples/Slf4jUser.java
@@ -4,10 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Slf4jUser {
-	private static final Logger log = LoggerFactory.getLogger(Slf4jUser.class);
+    private static final Logger log = LoggerFactory.getLogger(Slf4jUser.class);
 
-	public final String getMessage() {
-		log.debug("getMessage!");
-		return "log4j user";
-	}
+    public final String getMessage() {
+        log.debug("getMessage!");
+        return "log4j user";
+    }
 }
