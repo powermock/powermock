ad5ce445dc9706f7e26c94143e5313bb13f84869
Delyan Kratunov <delyank@fb.com>
2014-10-15 16:12:41 -0700

    Fix resource deferring to decouple tests from Surefire and Maven
    `MockClassLoaderTest` runs fine with Maven but fails with buck.
    The failure has to do with how it constructs a `URLClassLoader`.
    
    With Maven, the current ClassLoader is an `AppPathClassLoader` (since Maven
    runs the tests from an exploded folder tree) while with buck, it's a
    `JarClassLoader`. You can't specify a `URLClassLaoder` to a non-root location
    in a `jar`, it gets silently truncated to the root of the `jar`.
    
    Therefore, introduce `ResourcePrefixClassLoader` which prefixes resource paths
    (shocking, I know). This maintains the invariant that the test should fail to
    find the resources if `MockClassLoader` is not deferring correctly.

---
+++ core/src/test/java/org/powermock/core/classloader/ResourcePrefixClassLoader.java
@@ -0,0 +1,26 @@
+package org.powermock.core.classloader;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Enumeration;
+
+public class ResourcePrefixClassLoader extends ClassLoader{
+
+    private final String prefix;
+
+    public ResourcePrefixClassLoader(ClassLoader parent, String prefix) {
+        super(parent);
+        this.prefix = prefix;
+    }
+
+    @Override
+    protected Enumeration<URL> findResources(String name) throws IOException {
+        // default super behaviour returns null, we want to delegate to our parent, with a prefix
+        return getParent().getResources(this.prefix + name);
+    }
+
+    @Override
+    protected URL findResource(String name) {
+        return getParent().getResource(this.prefix + name);
+    }
+}
