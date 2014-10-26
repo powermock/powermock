13d1a4443d234404188644e48761c964c32034fe
Henrik Kaipe <kaipe@users.sourceforge.net>
2014-10-26 15:48:22 +0100

    TestParent is a trivial PowerMock test super-class that - when running tests with system-property "-Dpowermock.implicitDelegateAnnotation=true" - used to cause a test failure with the reasonable message "No runnable methods". The failure was fired by JUnit's default runner. This trivial problem was solved by making TestParent abstract.

--- modules/module-test/easymock/junit4-test/src/test/java/samples/junit4/testhierarchy/TestParent.java
+++ modules/module-test/easymock/junit4-test/src/test/java/samples/junit4/testhierarchy/TestParent.java
@@ -19,6 +19,6 @@ import org.junit.runner.RunWith;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 @RunWith(PowerMockRunner.class)
-public class TestParent {
+public abstract class TestParent {
 
 }
