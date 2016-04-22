00b5058257134faa806152241679167432aa917a
Tomasz Przybyla <bsodzik@tlen.pl>
2016-04-22 00:36:24 -0700

    Ensuring JVM method size limit is never exceeded (#661)
    Fix an issue when JVM limit is exceeded class loader is not able to load problematic class. This is manifested to user with enigmatic exception like:
    
       Exception in thread "main" java.lang.ClassFormatError: Invalid method Code length 91299 in class file com/test/SomeClass

---
+++ modules/module-test/mockito/junit4-agent/src/test/java/samples/powermockito/junit4/agent/LargeMethodTest.java
@@ -0,0 +1,58 @@
+package samples.powermockito.junit4.agent;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.rule.PowerMockRule;
+import samples.largemethod.MethodExceedingJvmLimit;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNull;
+import static junit.framework.Assert.assertSame;
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.fail;
+import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.verifyStatic;
+import static org.powermock.api.support.membermodification.MemberMatcher.method;
+import static org.powermock.api.support.membermodification.MemberModifier.suppress;
+
+@PrepareForTest(MethodExceedingJvmLimit.class)
+public class LargeMethodTest {
+
+    @Rule
+    public PowerMockRule powerMockRule = new PowerMockRule();
+
+    @Test
+    public void largeMethodShouldBeOverridden() {
+        try {
+            MethodExceedingJvmLimit.init();
+            fail("Method should be overridden and exception should be thrown");
+        } catch (Exception e) {
+            assertSame(IllegalAccessException.class, e.getClass());
+            assertTrue(e.getMessage().contains("Method was too large and after instrumentation exceeded JVM limit"));
+        }
+    }
+
+    @Test
+    public void largeMethodShouldBeAbleToBeSuppressed() {
+        suppress(method(MethodExceedingJvmLimit.class, "init"));
+        assertNull("Suppressed method should return: null", MethodExceedingJvmLimit.init());
+    }
+
+    @Test
+    public void largeMethodShouldBeAbleToBeMocked() {
+        mockStatic(MethodExceedingJvmLimit.class);
+        when(MethodExceedingJvmLimit.init()).thenReturn("ok");
+        assertEquals("Mocked method should return: ok", "ok", MethodExceedingJvmLimit.init());
+        verifyStatic();
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void largeMethodShouldBeAbleToBeMockedAndThrowException() {
+        mockStatic(MethodExceedingJvmLimit.class);
+        when(MethodExceedingJvmLimit.init()).thenThrow(new IllegalStateException());
+        MethodExceedingJvmLimit.init();
+        verifyStatic();
+    }
+}
