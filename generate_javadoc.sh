73f797e1f19b89b0e8f4077727c44297c9032c68
Johan Haleby <johan.haleby@gmail.com>
2016-04-29 13:40:18 +0200

    Added mockito2 and common library to javadoc generation

--- generate_javadoc.sh
+++ generate_javadoc.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 read -p "Version to generate javadoc for: " version
-project_names=(powermock-core powermock-api-easymock powermock-api-mockito powermock-reflect\
- powermock-api-support powermock-module-junit4-common powermock-module-junit4 powermock-module-junit4-rule\
+project_names=(powermock-core powermock-api-easymock powermock-api-mockito powermock-api-mockito2 powermock-api-mockito-common\
+ powermock-reflect powermock-api-support powermock-module-junit4-common powermock-module-junit4 powermock-module-junit4-rule\
  powermock-classloading-module powermock-classloading-xstream powermock-module-javaagent powermock-module-junit4-rule-agent\
  powermock-module-junit3 powermock-module-junit4-legacy powermock-module-testng-common powermock-module-testng\
  powermock-module-testng-agent powermock-modules-impl powermock-classloading-objenesis)
