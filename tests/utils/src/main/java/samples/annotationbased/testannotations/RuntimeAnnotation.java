9939fac82515b9138bb1c5cd86ebf8933c813bc0
Sebastian Saip <Sebastian.Saip@external.t-mobile.at>
2016-06-30 22:02:08 +0200

    Tests for #676

---
+++ tests/utils/src/main/java/samples/annotationbased/testannotations/RuntimeAnnotation.java
@@ -0,0 +1,12 @@
+package samples.annotationbased.testannotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value = {TYPE})
+public @interface RuntimeAnnotation {
+}
