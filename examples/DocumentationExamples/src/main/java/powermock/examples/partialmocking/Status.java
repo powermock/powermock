8c605c6fbcb74e726c0c53d17c9a417048b90c20
Arthur Zagretdinov <arthur.zagretdinov@outlook.com>
2016-12-03 15:09:36 +0100

    Move examples to a new repository
    Signed-off-by: Arthur Zagretdinov <arthur.zagretdinov@outlook.com>

--- examples/DocumentationExamples/src/main/java/powermock/examples/partialmocking/Status.java
+++
@@ -1,7 +0,0 @@
-package powermock.examples.partialmocking;
-
-/**
- * Created by Katharina Laube on 08.09.2014.
- */
-public class Status {
-}
--- examples/log4j/src/main/java/demo/org/powermock/examples/Log4jUserParent.java
+++
@@ -1,7 +0,0 @@
-package demo.org.powermock.examples;
-
-import org.apache.log4j.Logger;
-
-public class Log4jUserParent {
-	protected static final Logger log = Logger.getLogger(Log4jUserParent.class);
-}
