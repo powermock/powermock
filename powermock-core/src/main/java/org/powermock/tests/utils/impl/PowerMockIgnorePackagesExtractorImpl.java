4432467be1f4ae1c847693a9fc30b38e83c0e517
François JACQUES <hypnoce@donarproject.org>
2017-05-27 08:26:48 +0200

    Fixes #772. Do a DFS on superclass + interfaces hierarchy to find all PowerMockIgnore annotations. Avoid duplication introduced by diamond interface hierarchies by storing results in a set. (#777)

--- powermock-core/src/main/java/org/powermock/tests/utils/impl/PowerMockIgnorePackagesExtractorImpl.java
+++ powermock-core/src/main/java/org/powermock/tests/utils/impl/PowerMockIgnorePackagesExtractorImpl.java
@@ -20,15 +20,17 @@ import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.tests.utils.IgnorePackagesExtractor;
 
 import java.lang.reflect.AnnotatedElement;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class PowerMockIgnorePackagesExtractorImpl implements IgnorePackagesExtractor {
 
     @Override
     public String[] getPackagesToIgnore(AnnotatedElement element) {
-        List<String> ignoredPackages = new LinkedList<String>();
+        Set<String> ignoredPackages = new LinkedHashSet<String>();
         PowerMockIgnore annotation = element.getAnnotation(PowerMockIgnore.class);
         if (annotation != null) {
             String[] ignores = annotation.value();
@@ -36,10 +38,14 @@ public class PowerMockIgnorePackagesExtractorImpl implements IgnorePackagesExtra
         }
         if (element instanceof Class<?>) {
             Class<?> klazz = (Class<?>) element;
-            Class<?> superclass = klazz.getSuperclass();
-            if (superclass != null && !superclass.equals(Object.class)) {
-                String[] packagesToIgnore = getPackagesToIgnore(superclass);
-                Collections.addAll(ignoredPackages, packagesToIgnore);
+            Collection<Class<?>> superclasses = new ArrayList<Class<?>>();
+            Collections.addAll(superclasses, klazz.getSuperclass());
+            Collections.addAll(superclasses, klazz.getInterfaces());
+            for(Class<?> superclass : superclasses) {
+                if (superclass != null && !superclass.equals(Object.class)) {
+                    String[] packagesToIgnore = getPackagesToIgnore(superclass);
+                    Collections.addAll(ignoredPackages, packagesToIgnore);
+                }
             }
         }
         return ignoredPackages.toArray(new String[ignoredPackages.size()]);
