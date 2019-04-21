73c569f2a3a0552c54d88e020f0bb8686433ee81
Arthur Zagretdinov <a.zagretdinov@outlook.com>
2019-04-21 10:20:51 +0100

    [ci maven-central-release] Fixes #979 Generalise getting current java version to fix be able to use JavaAgent with latest java (#985)
    Fixes #979 Generalise getting current java version to fix be able to use Javaagent with the latest java

---
+++ powermock-core/src/main/java/org/powermock/utils/NumberUtils.java
@@ -0,0 +1,14 @@
+package org.powermock.utils;
+
+public class NumberUtils {
+    public static float toFloat(final String str, final float defaultValue) {
+        if (str == null) {
+            return defaultValue;
+        }
+        try {
+            return Float.parseFloat(str);
+        } catch (final NumberFormatException nfe) {
+            return defaultValue;
+        }
+    }
+}
---
+++ powermock-core/src/test/java/org/powermock/utils/JavaVersionTest.java
@@ -0,0 +1,16 @@
+package org.powermock.utils;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+
+public class JavaVersionTest {
+
+    @Test
+    public void should_return_true_for_current_version_that_if_high_than16() {
+        assertThat(JavaVersion.JAVA_RECENT.atLeast(JavaVersion.JAVA_1_6))
+                .isTrue();
+    }
+
+}
